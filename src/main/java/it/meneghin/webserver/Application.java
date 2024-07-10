9b27035547ddc27effb5cf86062ca4d36c5679e3
Meneghin <simone.meneghin.98@outlook.it>
2024-07-10 16:00:25 +0200

    added logger

--- src/main/java/it/meneghin/webserver/Application.java
+++ src/main/java/it/meneghin/webserver/Application.java
@@ -1,8 +1,17 @@
 package it.meneghin.webserver;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
 public class Application {
 
+	private static final Logger log = LogManager.getLogger(Application.class);
+
 	public static void main(String[] args) {
-		System.out.println("INIT");
+		log.trace("TRACE");
+		log.debug("DEBUG");
+		log.info("INFO");
+		log.error("ERROR");
 	}
 }
