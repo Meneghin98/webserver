01067e77235f732ff4082660b753a51433de21c2
Simone Meneghin <simone.meneghin.98@outlook.it>
2024-11-09 11:51:26 +0100

    start refactor to websocket protocol

--- src/main/java/it/meneghin/webserver/core/handlers/SocketHandler.java
+++ src/main/java/it/meneghin/webserver/core/handlers/SocketHandler.java
@@ -3,8 +3,7 @@ package it.meneghin.webserver.core.handlers;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
@@ -14,32 +13,63 @@ public class SocketHandler implements Runnable {
     private static final Logger log = LogManager.getLogger(SocketHandler.class);
 
     private final Socket socket;
+    private BufferedReader buffInReader;
+    private BufferedWriter buffOutWriter;
 
+    //TODO: socket not null
     public SocketHandler(Socket socket) {
         this.socket = socket;
+    }
+
+    private void setup() throws IOException {
+        buffInReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        buffOutWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    }
+
+    private void teardown() {
+        try {
+            if(buffOutWriter != null) buffOutWriter.close();
+            if(buffInReader != null) buffInReader.close();
+            socket.close();
+        } catch (IOException e) {
+            log.error("SocketHandler teardown", e);
+        }
+    }
+
+    private String getReqestLine() {
+        return buffInReader.readLine();
     }
 
     @Override
     public void run() {
         try{
-            OutputStream os = socket.getOutputStream();
+            setup();
 
-            String responseContent = "<html><body>Hello!</body></html>";
-            String response = "HTTP/1.1 200 OK" +CRLF+
-                    "Content-Type: text/html; charset=UTF-8"+CRLF+
-                    "Content-Length: " +
-                    responseContent.getBytes(StandardCharsets.UTF_8).length +CRLF+
-                    CRLF+
-                    responseContent;
+            String requestLine = buffInReader.readLine(); //TODO: request line
 
-            os.write(response.getBytes(StandardCharsets.UTF_8));
-            log.debug("Written response:\n{}", response);
+            for (String line = ""; !CRLF.equals(line); line = buffInReader.readLine()) {
 
-            os.close();
-            socket.close();
+            }
+
+//            OutputStream outStream = socket.getOutputStream();
+//
+//            String responseContent = "<html><body>Hello!</body></html>";
+//            String response = "HTTP/1.1 101 OK" +CRLF+
+//                    "Content-Type: text/html; charset=UTF-8"+CRLF+
+//                    "Content-Length: " +
+//                    responseContent.getBytes(StandardCharsets.UTF_8).length +CRLF+
+//                    CRLF+
+//                    responseContent;
+//
+//            os.write(response.getBytes(StandardCharsets.UTF_8));
+//            log.debug("Written response:\n{}", response);
+//
+//            os.close();
 
         } catch (IOException e) {
             log.error("Socket processing has encountered an error", e);
+        } finally {
+            teardown();
         }
     }
 }
